6c8720e3cfccc1b21dfe1e2cc89cc16e3f36f7fc
alex <alex@ALEX-PC>
2016-09-04 21:48:35 +0100

    Add project files.

---
+++ Planit01/Planit01/HomePage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Planit01
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            //ToDo
+            // Figure out logic to get phone number + imei number from OS and login automatically
+            // If already authenticated
+
+            // Login Button Clicked
+            loginButton.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new LoginPage());
+            };
+
+            // Join Button Clicked
+            joinButton.Clicked += async (sender, args) =>
+            {
+                await Navigation.PushAsync(new JoinPage());
+            };
+
+        }
+    }
+}
