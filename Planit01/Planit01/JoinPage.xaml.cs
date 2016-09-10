7e9b986c40392e7c5d9dc05b8069b27cfc492dbf
alex <alex.grover@outlook.com>
2016-09-10 17:49:36 +0100

    join page UI and logic done. Need to wire up to server

--- Planit01/Planit01/JoinPage.xaml.cs
+++ Planit01/Planit01/JoinPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,79 @@ namespace Planit01
 {
     public partial class JoinPage : ContentPage
     {
+
+        bool newPhoto = false;
+
         public JoinPage()
         {
             InitializeComponent();
+
+            inputImage.Source = ImageSource.FromFile("Images/default-user-image.png");
         }
+
+        async void OnJoinButtonClicked(object sender, EventArgs args)
+        {
+            // Send number to WebAPI
+            var phoneNumber = inputNumber.Text;
+
+            bool foundNumber = true;
+
+            // -- All done on back end --
+            // isNumber in DB
+            //foundNumber = 
+
+            if (foundNumber)
+            {
+                PhoneNumberAlreadyExists();
+            }
+            else
+            {
+                var userName = inputName.Text;
+                if (newPhoto)
+                {
+                    // User has chosen a new image
+                    var image = inputImage;
+                }
+                
+                // send json to server to create user
+                // Response received from server that user has been created
+
+                // Go to profile page
+                await Navigation.PushAsync(new ProfilePage());
+
+            }
+        }
+
+        async void OnPhotoTapped(object sender, EventArgs args)
+        {
+            // Select a photo from gallery 
+            if (CrossMedia.Current.IsPickPhotoSupported)
+            {
+                var photo = await CrossMedia.Current.PickPhotoAsync();
+                inputImage.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = photo.GetStream();
+                    photo.Dispose();
+                    return stream;
+                });
+
+                newPhoto = true;
+            }
+                
+        }
+
+        async void OnLoginButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+
+        private void PhoneNumberAlreadyExists()
+        {
+            labelUserAlreadyExists.IsEnabled = true;
+            labelUserAlreadyExists.IsVisible = true;
+            buttonLogin.IsEnabled = true;
+            buttonLogin.IsVisible = true;
+        }
+
     }
 }
