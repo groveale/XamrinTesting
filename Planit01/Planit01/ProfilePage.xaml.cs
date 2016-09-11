a677797f68433291d65fadf452c592dac0ef7f42
alex <alex.grover@outlook.com>
2016-09-11 22:12:25 +0100

    Profile Page skeleton complete - Need to wire up

--- Planit01/Planit01/ProfilePage.xaml.cs
+++ Planit01/Planit01/ProfilePage.xaml.cs
@@ -13,6 +13,49 @@ namespace Planit01
         public ProfilePage()
         {
             InitializeComponent();
+
+            imageProfile.Source = ImageSource.FromFile("Images/default-user-image.png");
+        }
+
+        async void OnPhotoTapped(object sender, EventArgs args)
+        {
+            /*
+            // Select a photo from gallery 
+            if (CrossMedia.Current.IsPickPhotoSupported)
+            {
+                var photo = await CrossMedia.Current.PickPhotoAsync();
+                inputImage.Source = ImageSource.FromStream(() =>
+                {
+                    Stream photoStream = photo.GetStream();
+                    return photoStream;
+                });
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    photo.GetStream().CopyTo(ms);
+                    photo.Dispose();
+                    userImage = ms.ToArray();
+                }
+            }
+            */
+        }
+
+        async void OnCreateEventButtonClicked(object sender, EventArgs args)
+        {
+            // New Event Page
+            //await Navigation.PushAsync(new LoginPage());
+        }
+
+        async void OnGroupButtonClicked(object sender, EventArgs args)
+        {
+            // Go To Group Page
+            //await Navigation.PushAsync(new LoginPage());
+        }
+
+        async void OnCalendarButtonClicked(object sender, EventArgs args)
+        {
+            // Go to calendar page
+            //await Navigation.PushAsync(new LoginPage());
         }
     }
 }
